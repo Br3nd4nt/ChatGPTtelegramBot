fa581315831c1a82cc027e4e21e333f5dd311097
Vladislav Belov <vmbelov_3@edu.hse.ru>
2023-05-26 13:21:41 +0300

    fixed long dialog bug, trying reboot logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 import telebot
 from telebot import custom_filters
@@ -14,10 +15,18 @@ bot = telebot.TeleBot(tgToken)
 
 userMessages = dict()
 
+def count_tokens(messages):
+    tokens = 0
+    for message in messages:
+        if message["role"] == "user":
+            tokens += len(message["content"].split(" "))
+    return tokens
+
 @bot.message_handler(text_startswith="!")
 def start_filter(message):
     logging.info(f"{message.from_user.username}: {message.text}")
-
+    logging.info(f"{message.from_user.username} tokens: {count_tokens(userMessages[message.from_user.username])}")
+    print(message.from_user.id)
     if message.text.startswith("!generate "):
         prompt = message.text[10:]
         try:
@@ -40,17 +49,25 @@ def start_filter(message):
                     messages = userMessages[message.from_user.username]
                 except KeyError:
                     messages = []
+                
+                while count_tokens(messages) > 4000:
+                    messages.pop(0)
+                    messages.pop(0)
+                
                 messages.append({"role": "user", "content": message.text[1:]})
                 completions = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo", 
                 messages=messages
                 )
+
                 respond = completions.choices[0]["message"]["content"]
                 bot.reply_to(message, respond)
                 messages.append(completions.choices[0]["message"])
                 userMessages[message.from_user.username] = messages
+
             else:
-                respond = res = openai.Completion.create(
+
+                respond = openai.Completion.create(
                 model="text-davinci-003",
                 prompt=message.text[1:],
                 max_tokens=1024,
@@ -62,8 +79,14 @@ def start_filter(message):
             logging.error(e)
             bot.reply_to(message, "Connection to OpenAI timed out :(")
 
-
 bot.add_custom_filter(custom_filters.TextStartsFilter())
-print("started")
-import asyncio
-asyncio.run(bot.polling(skip_pending=True))
+
+if __name__ == "__main__":
+    bot.send_message(496270846, "bot deployed")
+    import asyncio
+    while True:
+        try:
+            asyncio.run(bot.polling(skip_pending=True))
+        except Exception as e:
+            logging.error(e)
+            bot.send_message(496270846, "fuckin bitch crashed, trying to reboot")
